76c113a608a06b1c66fa292a54ec571af52e040d
J Freegman <JFreegman@gmail.com>
2012-09-20 01:26:57 -0400

    v3.1

--- buttdestroyer.py
+++ buttdestroyer.py
@@ -1,130 +1,44 @@
+# -----------------------------------------------------------------------------------
+#
 # Author: JFreegman
 # Contact: JFreegman@gmail.com
-# Date: July 25, 2012
-# v1.1
-# Update from v1.0: pattern recognition improved and optimized
-
-# This is the BUTT DESTROYER 9001 v1.1, a rock paper scissors playing bot 
-# originally written for Udacity's CS212 rock, paper scissors tournament and 
-# adapted for RPSContest.com.
-
+# Date: July 30, 2012
+# v3.1
+# 
+# This is a rock-paper-scissors playing bot originally written for the Udacity CS212
+# RPS contest and adapted for tournament play at rpscontest.com
+#
+# See: http://www.rpscontest.com/authorSearch?name=JFreegman
+# 
 # All code is written from scratch. The general idea is based off Iocaine Powder 
-# by Dan Egnor (http://ofb.net/~egnor/iocaine.html).
-
-# What this bot attempts to do is create a prediction heuristic based on
-# frequency of the opponent's moves and patterns in their history. The bot will
-# keep track of the performance of a number of strategies and meta-strategies 
-# while playing the move that its current most successfull strategy comes up with.
+# by Dan Egnor -- http://ofb.net/~egnor/iocaine.html
+#
+# -----------------------------------------------------------------------------------
 
 import random
 
-# Keeps history of each strategy's move for each round
-STRAT_HISTORY = []
-
-# Keeps track of the success rate of each strategy
-STRAT_SUCCESS = {'freq20': 0, 'c1_freq20': 0, 'c2_freq20': 0, 'hist': 0, 'random': 0,
-                 'c1_hist': 0, 'c2_hist': 0, 'c1_freq100': 0, 'c2_freq100': 0,
-                 'freq100': 0, 'freqtot': 0, 'c1_freqtot': 0, 'c2_freqtot': 0}
-
-# Keeps track of opponent move patterns
-PATTERNS = ""
-
-def player(my_moves, opp_moves):
-    global PATTERNS
-    # first move is always random
-    if not my_moves:
-        STRAT_HISTORY.append({})
-        return random_weapon()
-
-    # update strategy success rates based on last round results
-    last_opp_move = opp_moves[-1]
-    beat_opp = winning_move(last_opp_move)
-    lose_opp = losing_move(last_opp_move)
-    last_strats = STRAT_HISTORY[-1]
-    for s in last_strats:
-        if last_strats[s] == beat_opp:
-            STRAT_SUCCESS[s] += 1
-        elif last_strats[s] == lose_opp:
-            STRAT_SUCCESS[s] -= 1
-
-    PATTERNS += last_opp_move
-
-    # get opponent's most probable move based on frequency (last 20, 100 and
-    # full history) and history pattern matches
-    opp_freq20 = get_probs(opp_moves, 20)
-    opp_prob_f_20 = max(opp_freq20, key=opp_freq20.get)
-    opp_freq100 = get_probs(opp_moves, 100)
-    opp_prob_f_100 = max(opp_freq100, key=opp_freq100.get)
-    opp_freqtot = get_probs(opp_moves, len(opp_moves))
-    opp_prob_f_tot = max(opp_freqtot, key=opp_freqtot.get)
-    opp_prob_h = get_history_match()
-
-    # naive moves for each strategy
-    my_move_freq20 = winning_move(opp_prob_f_20)
-    my_move_freq100 = winning_move(opp_prob_f_100)
-    my_move_freqtot = winning_move(opp_prob_f_tot)
-    my_move_hist = winning_move(opp_prob_h)
-    random_move = random_weapon()
-
-    # counter-moves in case opp predicts my naive moves
-    c1_move_freq20 = losing_move(my_move_freq20)
-    c1_move_freq100 = losing_move(my_move_freq100)
-    c1_move_freqtot = losing_move(my_move_freqtot)
-    c1_move_hist = losing_move(my_move_hist)
-
-    # counter-counter moves in case opp predicts my first counter
-    c2_move_freq20 = losing_move(c1_move_freq20)
-    c2_move_freq100 = losing_move(c1_move_freq100)
-    c2_move_freqtot = losing_move(c1_move_freqtot)    
-    c2_move_hist = losing_move(c1_move_hist)
-
-    # dict of all available strategies and their move
-    strats = {'freq20': my_move_freq20, 'freq100': my_move_freq100, 
-              'hist': my_move_hist, 'random': random_move,
-              'c1_freq20': c1_move_freq20, 'c2_freq20': c2_move_freq20, 
-              'c1_freq100': c1_move_freq100, 'c2_freq100': c2_move_freq100, 
-              'c1_hist': c1_move_hist, 'c2_hist': c2_move_hist,
-              'freqtot': my_move_freqtot, 'c1_freqtot': c1_move_freqtot,
-              'c2_freqtot': c2_move_freqtot}
-
-    # Pick the strategy with the highest current success rate
-    strat = max(strats, key=lambda x: STRAT_SUCCESS[x])
-    move = strats[strat]
-    STRAT_HISTORY.append(strats)
-    return move
-    
-def get_history_match():
-    """
-    Searches moves history and tries to find previous patterns that match
-    the last n moves (n starting at half the number of moves and decrementing).
-    If match found, returns the move made after the last pattern match.
-    """
-    start = len(PATTERNS) - min(len(PATTERNS) / 2, 200)
-    end = len(PATTERNS)
+def get_history_match(hist, n=15, s=False):
+    start = len(hist) - min(len(hist) / 2, n)
+    end = len(hist)
     for i in xrange(start, end):
-        partition = PATTERNS[i:end]
-        match = PATTERNS[:-1].find(partition)
+        partition = hist[i:end]
+        match = hist[:-1].rfind(partition)
         if match != -1:
-            return PATTERNS[match+len(partition)]
-    return random_weapon()
+            return hist[match+len(partition)]
+    if not s:
+        return random_weapon()
+    return None
 
 def get_probs(total_moves, n):
-    """
-    Returns a dictionary containing the probabilities that the opponent
-    will make a given move based on their last n moves.
-    """
     last = get_move_freq(total_moves[-n:])
     probs = {}
-    probs['R'] = float(last['R']) / last['total'] * 100
-    probs['S'] = float(last['S']) / last['total'] * 100
-    probs['P'] = float(last['P']) / last['total'] * 100
+    t = last['total']
+    probs['R'] = float(last['R']) / t
+    probs['S'] = float(last['S']) / t
+    probs['P'] = float(last['P']) / t
     return probs
 
 def get_move_freq(moves):
-    """
-    Returns a dictionary containing frequencies of moves, as well as
-    a count for the total number of moves
-    """
     mov_freq = {'R': 0, 'P': 0, 'S': 0}
     count = 0
     for move in moves:
@@ -140,19 +54,87 @@ def get_move_freq(moves):
     mov_freq['total'] = count
     return mov_freq
 
-def winning_move(m):
-    "Returns the move that beats m"
-    d = {'R': 'P', 'P': 'S', 'S': 'R'}
-    return d[m]
-    raise ValueError, 'Invalid move'
-
-def losing_move(m):
-    "Returns the move that loses to m"
-    d = {'R': 'S', 'P': 'R', 'S': 'P'}
-    return d[m]
-    raise ValueError, 'Invalid move'
-
 def random_weapon():
-    "Randomly chooses R, P or S"
-    moves = ['R', 'P', 'S']
-    return random.choice(moves)
+    return random.choice(['R', 'P', 'S'])
+
+# Initialize everything on the first move
+if not input:
+    last_strats = {}
+
+    winning_move = {'R': 'P', 'P': 'S', 'S': 'R'}
+
+    losing_move = {'R': 'S', 'P': 'R', 'S': 'P'}
+
+    plays = {'RR': 'W', 'RP': 'G', 'RS': 'E', 'PP': 'Q', 'PR': 'L', 
+             'PS': 'B', 'SS': 'C', 'SR': 'A', 'SP': 'J'}
+
+    opposite_plays = {'W': 'RR', 'G': 'RP', 'E': 'RS', 'Q': 'PP', 'L': 'PR',
+                      'B': 'PS', 'C': 'SS', 'A': 'SR', 'J': 'SP'}
+
+    strat_success = {'freq20': 0,'hist': 0, 'random': 0, 'freq100': 0, 
+                     'my_hist': 0, 'c_my_seq': 0, 'c1_my_seq': 0,
+                     'c_my_move_my_hist': 0, 'c1_my_move_my_hist': 0,
+                     'opp_seq': 0, 'my_seq': 0, 'c_opp_seq': 0, 'c1_opp_seq': 0,
+                     }
+    output = random_weapon()
+    opp_moves = ""
+    sequences = ""
+    my_moves = output
+
+else:
+    opp_moves += input
+    last_opp_move = input
+    sequences += plays[input+output]
+    beat_opp = winning_move[last_opp_move]
+    lose_opp = losing_move[last_opp_move]
+    # update strategy success rates based on last round results
+    for s in last_strats:
+        if last_strats[s] == beat_opp:
+            strat_success[s] += 1
+        elif last_strats[s] == lose_opp:
+            strat_success[s] -= 1
+
+    # opponent's most probable move based on frequency and historic patterns
+    opp_freq20 = get_probs(opp_moves, 20)
+    opp_prob_f_20 = max(opp_freq20, key=opp_freq20.get)
+    opp_freq100 = get_probs(opp_moves, 100)
+    opp_prob_f_100 = max(opp_freq100, key=opp_freq100.get)
+    opp_prob_h = get_history_match(opp_moves)
+
+    # my most probable move based on history
+    my_prob_h = get_history_match(my_moves)
+
+    # moves for each strategy
+    my_move_freq20 = winning_move[opp_prob_f_20]
+    my_move_freq100 = winning_move[opp_prob_f_100]
+    my_move_hist = winning_move[opp_prob_h]
+    my_move_my_hist = losing_move[my_prob_h]
+    c_my_move_my_hist = losing_move[my_move_my_hist]
+    c1_my_move_my_hist = losing_move[c_my_move_my_hist]
+    random_move = random_weapon()
+
+    strats = {'freq20': my_move_freq20, 'freq100': my_move_freq100, 
+              'hist': my_move_hist, 'random': random_move,
+              'c1_my_move_my_hist': c1_my_move_my_hist, 'my_hist': my_move_my_hist,
+              'c_my_move_my_hist': c_my_move_my_hist,
+              }
+
+    # most probable moves based on sequence pattern matches
+    seq_match = get_history_match(sequences, s=True)
+    if seq_match:
+        opp_prob_seq = winning_move[opposite_plays[seq_match][0]]
+        my_prob_seq = losing_move[opposite_plays[seq_match][1]]
+        strats['opp_seq'] = opp_prob_seq
+        strats['c_opp_seq'] = losing_move[opp_prob_seq]
+        strats['c1_opp_seq'] = winning_move[opp_prob_seq]
+        strats['my_seq'] = my_prob_seq
+        strats['c_my_seq'] = losing_move[my_prob_seq]
+        strats['c1_my_seq'] = winning_move[my_prob_seq]
+        
+    # Pick the strategy with the highest current success rate
+    strat = max(strats, key=lambda x: strat_success[x])
+    output = strats[strat]
+    if random.random() <= .1:
+        output = random_weapon()
+    my_moves += output
+    last_strats = strats
